019d7432b38d3042dbee476388a62ecc1ab11e84
Bahriddin1234 <bahriddinabdusalomov07@gmail.com>
2023-06-21 10:28:04 +0500

    Created collections

---
+++ HomeTask_19_06_2023/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime;
+
+namespace HomeTask_19_06_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Toplam<int> toplam = new Toplam<int>();
+
+            Console.Write("Toplam elementlarini kiriting: ");
+
+            string[] strings = Console.ReadLine().Split();
+            int i = 0;
+
+            foreach (var item in strings)
+                 toplam.Qosh(int.Parse(item));
+
+
+            int max = toplam[0];
+            for (i = 0; i < toplam.Soni; i++)
+            {
+                if (toplam[i] > max) max = toplam[i];
+            }
+
+            toplam.Ochir(max);
+
+            Console.WriteLine("Toplamning eng katta elementi ochirildi.. ");
+
+            for (i = 0; i < toplam.Soni; i++)
+            {
+                Console.Write(toplam[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ HomeTask_19_06_2023/Toplam.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_19_06_2023
+{
+    public class Toplam<T>
+    {
+        private List<T> _number = new List<T>();
+
+        public T this[int index]
+        {
+            get
+            {
+                return _number[index];
+            }
+            set
+            {
+                _number[index] = value;
+            }
+        }
+
+        public void Qosh(T value)
+        {
+            _number.Add(value);
+        }
+
+        public void Ochir(T value)
+        {
+            _number.Remove(value);
+        }
+
+        public  int Soni
+        {
+            get { return _number.Count; }
+        }
+
+    }
+}
