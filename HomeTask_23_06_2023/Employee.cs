bee8b064f5af34d830aa357d637b3f04ceb94b0e
Bahriddin1234 <bahriddinabdusalomov07@gmail.com>
2023-06-24 17:22:30 +0500

    Directory and File

---
+++ HomeTask_23_06_2023/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_23_06_2023
+{
+    public class Employee
+    {
+
+        public Guid Id { get; set; }    
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public Employee(string firstName, string lastName, string phoneNumber, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public static void AddFiles(string basePath, string fileName, List<string> files)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                string path = Path.Combine(basePath, fileName + $"{i}.txt");
+                files.Add(path);
+
+                FileInfo fileInfo = new FileInfo(path);
+
+            }
+        }
+
+        public static void AddUserToFile(string path, Employee employee)
+        {
+            string employeeInfo = $"First name: {employee.FirstName}" +
+                $"\nLast name: {employee.LastName}" +
+                $"\nPhone number: {employee.PhoneNumber}" +
+                $"\nEmail address: {employee.Email}";
+
+            File.WriteAllText(path, employeeInfo);
+        }
+    }
+}
---
+++ HomeTask_23_06_2023/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+
+namespace HomeTask_23_06_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Directory.CreateDirectory("CreateDirectory");
+
+            string fileName = "file";
+            string basePath = "C:\\Users\\USER\\Desktop\\Dasturlash\\DotNetBootcamp\\HomeTask-Group-N7\\HomeTask_23_06_2023\\bin\\Debug\\net7.0\\CreateDirectory";
+
+            List<string> files = new List<string>();
+
+            // Bu method Directoriyaga file qoshib beradi.
+
+            Employee.AddFiles(basePath, fileName, files);
+
+            List<Employee> employees = new List<Employee>();
+
+            employees.AddRange(new[]
+            {
+                new Employee("Sanjarbek", "Nasvaliyev", "944390120", "sanjarbek@gamil.com"),
+                new Employee("Jasur", "Ubaydullayev", "932220553", "jasur@gmail.com"),
+                new Employee("Hamdam", "Nosirov", "901911232", "hamdam@gmail.com"),
+                new Employee("Lazizbek", "Xoshiov", "991233421", "laziz@gmail.com"),
+                new Employee("Shoxrux", "Xotamov", "930912003", "shoxrux@gmail.com"),
+                new Employee("Jamshid", "Asadov", "901212234", "jamshid@gmail.com"),
+                new Employee("Ijodbek", "Abdurahmonov", "906080208", "ijod@gmail.com"),
+                new Employee("Bahriddin", "Abdusalomov", "944396669", "bahriddinabdusalomov@gmail.com"),
+                new Employee("Davronbek", "Toxtamatov", "945552905", "davron@gmail.com"),
+                new Employee("Arabboy", "Xamraliyev", "9455055150", "arabboy@gmail.com")
+            });
+
+            int count = 0;
+            foreach (var item in employees)
+            {
+                Employee.AddUserToFile(files[count++], item);
+            }
+
+        }
+    }
+}
